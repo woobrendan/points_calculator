543a2f058860657b68846ed31f5f5ae46e10de18
Brendan Woo <woo.brendan@gmail.com>
2023-06-19 16:34:22 -0400

    create func to take in a file path and return the fully cleaned data. add in additional 3 into change_key_name

--- test.py
+++ test.py
@@ -2,14 +2,17 @@ import csv
 
 file_path = './Results/results.csv'
 
-def display_data(file_path):
-    with open(file_path, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            if row['Pos']:
-                print(row)
+key_list = [
+    'Pos',
+    'PIC',
+    '#',
+    'Name',
+    'Class',
+    'Points',
+    'Additional3',
+    'Additional4'
+]
 
-# display_data(file_path)
 
 def csv_to_dict_arr(file_path):
     with open(file_path, 'r') as csv_file:
@@ -19,17 +22,6 @@ def csv_to_dict_arr(file_path):
             results.append(row)
         return results
 
-# csv_to_dict_arr(file_path)
-
-key_list = [
-    'Pos',
-    'PIC',
-    '#',
-    'Name',
-    'Class',
-    'Points',
-    'Additional4'
-]
 
 def clean_results(arr, key_arr):
     results = []
@@ -38,16 +30,32 @@ def clean_results(arr, key_arr):
         results.append(filtered)
     return results
 
+
 cleaned = clean_results(csv_to_dict_arr(file_path), key_list)
 
+
 def change_key_name(dict_arr):
-    for result in dict_arr: 
+    for result in dict_arr:
         if 'Name' in result:
             result['Team'] = result.pop('Name')
         if 'Additional4' in result:
             result['Vehicle'] = result.pop('Additional4')
+        if 'Additional3' in result:
+            result['Series'] = result.pop('Additional3')
     return dict_arr
 
-# print(clean_results(csv_to_dict_arr(file_path), key_list))
-for row in change_key_name(cleaned):
+
+# returns cleaned up data  to use to compare to entries
+def csv_to_clean_keys(file_path):
+    dict_arr = csv_to_dict_arr(file_path)
+    cleaned = clean_results(dict_arr, key_list)
+    return change_key_name(cleaned)
+
+
+for row in csv_to_clean_keys(file_path):
     print(row)
+
+
+# print(clean_results(csv_to_dict_arr(file_path), key_list))
+# for row in change_key_name(cleaned):
+#     print(row)
