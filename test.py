7950950920ff0a183057b54b6f63b32c960f4b35
Brendan Woo <woo.brendan@gmail.com>
2023-06-19 14:50:24 -0400

    create func to return arr of results

--- test.py
+++ test.py
@@ -9,4 +9,14 @@ def display_data(file_path):
             if row['Pos']:
                 print(row)
 
-display_data(file_path)
+# display_data(file_path)
+
+def csv_to_dict(file_path):
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        results = []
+        for row in csv_reader:
+            results.append(row)
+        return results
+
+csv_to_dict(file_path)
