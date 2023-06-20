82c1f2ebe55ee89a49fb85306873fb2f58c144fc
Brendan Woo <woo.brendan@gmail.com>
2023-06-20 15:02:46 -0700

    add in file path and import scv to clearn keys. create variables for result and entries, create loop to add points to matching entries

--- Front_end/test.py
+++ Front_end/test.py
@@ -1,4 +1,7 @@
 import requests
+from csv_converter import csv_to_clean_keys
+
+file_path = '../Results/results.csv'
 
 
 def get_entries():
@@ -7,5 +10,21 @@ def get_entries():
     return data['entry']
 
 
-for row in get_entries():
-    print(row)
+# for row in get_entries():
+#     print(row)
+result = csv_to_clean_keys(file_path)
+entries = get_entries()
+
+if __name__ == "__main__":
+    for row in result:
+        for entry in entries:
+            if row['#'] == entry['number']:
+                points = int(row['Points'])
+                entry['driver1']['points'] += points
+                entry['driver1']['points'] += points
+                print(entry)
+                # Beware of double loop, printed twice
+                # hit post route to update points
+
+    # for row in result:
+    #     print(row)
