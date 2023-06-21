3491105387ed707af102d362e1956d2b45f0ed59
Brendan Woo <woo.brendan@gmail.com>
2023-06-21 16:31:46 -0700

    update to use entry mapping and creating a new dictionary to efficiently find the matching entry

--- Front_end/update_points.py
+++ Front_end/update_points.py
@@ -16,22 +16,23 @@ result = csv_to_clean_keys(file_path)
 entries = get_entries()
 
 if __name__ == "__main__":
+    # creates a dictionary, with the key as the number and series, and the value as the entry
+    entry_mapping = {(entry['number'], entry['series'])
+                      : entry for entry in entries}
+
     for row in result:
-        for entry in entries:
-            if row['#'] == entry['number']:
-                points = int(row['Points'])
-                # update matching entry points for each driver
-                entry['driver1']['points'] += points
+        key = (row['#'], row['Series'])
+        if key in entry_mapping:
+            entry = entry_mapping[key]
+            points = int(row['Points'])
+            entry['driver1']['points'] += points
 
-                driver2 = entry.get('driver2')
-                if driver2:
-                    entry['driver2']['points'] += points
+            driver2 = entry.get('driver2')
+            if driver2:
+                driver2['points'] += points
 
-                update_url = 'http://localhost:2020/entries/' + entry['_id']
-                requests.patch(update_url, json=entry)
-
-                # Beware of double loop, printed twice
-                # hit post route to update points
+            update_url = 'http://localhost:2020/entries/' + entry['_id']
+            requests.patch(update_url, json=entry)
 
     # for row in result:
     #     print(row)
