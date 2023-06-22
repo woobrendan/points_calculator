f4ab6c27a0432df2dce97bc9678f830260e26c41
Brendan Woo <woo.brendan@gmail.com>
2023-06-22 11:03:46 -0700

    add in standing_list for list of standing types, and pass variable to the frontend

--- points_calculator/core/views.py
+++ points_calculator/core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    standing_list = ('Drivers', 'Manufacturer', 'Team')
+    return render(request, 'core/index.html', {
+        'standing_list': standing_list
+    })
