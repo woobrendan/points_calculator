a5728ee5acef81604a4c00376f4e4978dab541bb
Brendan Woo <woo.brendan@gmail.com>
2023-06-22 17:31:34 -0700

    update secret key to use env variable, add core and entries to installed apps

--- points_calculator/points_calculator/settings.py
+++ points_calculator/points_calculator/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qy-*7e44bz*!b1ylz*_q(ly4n-n^hjsa_gypa8zhynz_8go#ya'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'entries'
 ]
 
 MIDDLEWARE = [
