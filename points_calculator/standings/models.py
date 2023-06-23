23b3679d88d14e3f6e75ce0ee06c511fccd2344b
Brendan Woo <woo.brendan@gmail.com>
2023-06-23 13:49:38 -0700

    fix tuple append comma

--- points_calculator/standings/models.py
+++ points_calculator/standings/models.py
@@ -5,7 +5,7 @@ class Standing(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ('name')
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
