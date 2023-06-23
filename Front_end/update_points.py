0aff22b41f46dff753d97b5de2ce20590683c411
Brendan Woo <woo.brendan@gmail.com>
2023-06-23 13:47:31 -0700

    update logic to handle total points addition, and create list of points. add else block into update points to appending None to list. add paramter entry to update poiints. inside main func, add loop through entries and pass entry into udpate points

--- Front_end/update_points.py
+++ Front_end/update_points.py
@@ -14,27 +14,38 @@ result = csv_to_clean_keys(file_path)
 entries = get_entries()
 
 
-def update_points():
+def update_points(entry):
     # creates a dictionary, with the key as the number and series, and the value as the entry
-    entry_mapping = {(entry['number'], entry['series'])                     : entry for entry in entries}
+    entry_mapping = {(entry['number'], entry['series']): entry for entry in entries}
 
     for row in result:
         key = (row['#'], row['Series'])
+        driver2 = entry.get('driver2')
+
         if key in entry_mapping:
             entry = entry_mapping[key]
             points = int(row['Points'])
-            entry['driver1']['points'] += points
 
-            driver2 = entry.get('driver2')
+            entry['driver1']['totalPoints'] += points
+            entry['driver1']['points'].append(points)
+
             if driver2:
-                driver2['points'] += points
+                entry['driver2']['totalPoints'] += points
+                entry['driver2']['points'].append(points)
 
-            update_url = 'http://localhost:2020/entries/' + entry['_id']
-            requests.patch(update_url, json=entry)
+        else:
+            entry['driver1']['points'].append(None)
+
+            if driver2:
+                entry['driver2']['points'].append(None)
+
+        update_url = 'http://localhost:2020/entries/' + entry['_id']
+        requests.patch(update_url, json=entry)
 
 
 # for row in get_entries():
 #     print(row)
 
 if __name__ == "__main__":
-    update_points()
+    for entry in entries:
+        update_points(entry)
