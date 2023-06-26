12ab9d3ea7b9ce190e3f7f566fb86f49664254da
Brendan Woo <woo.brendan@gmail.com>
2023-06-26 15:03:13 -0700

    create driver stnading controller. fetch from api get entries, then filter based on series value

--- points_calculator/standings/views.py
+++ points_calculator/standings/views.py
@@ -1,3 +1,21 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def drivers_standing():
+    response = requests.get('http://localhost:2020/entries')
+
+    if response.status_code == 201:
+        data = response.json()['entry']
+        # Refactor later for series to be query from front end
+        series = 'GT World Challenge America'
+        filtered = [entry for entry in data
+                    if entry.get('series') == series]
+
+    else:
+        return HTTPResponse('Failed to fetch data from API')
+
+
+if __name__ == "__main__":
+    drivers_standing()
