5fc10407c0f9811b8a252a370726456098668116
Brendan Woo <woo.brendan@gmail.com>
2023-06-26 15:13:26 -0700

    add route middle ware for standings url path

--- points_calculator/points_calculator/urls.py
+++ points_calculator/points_calculator/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('standings/', include('standings.urls')),
     path('admin/', admin.site.urls),
 ]
