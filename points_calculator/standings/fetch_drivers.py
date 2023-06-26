f4d6a9a36cada7c028151d2c82b7295a19340c52
Brendan Woo <woo.brendan@gmail.com>
2023-06-26 15:42:42 -0700

    create logic to loop through all entries, create a dictionary with class as keys and list of entries. then loop through each entry list and return a list of drivers for that class

--- points_calculator/standings/fetch_drivers.py
+++ points_calculator/standings/fetch_drivers.py
@@ -1,6 +1,7 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 import requests
+import json
 
 
 def fetch_drivers():
@@ -12,17 +13,39 @@ def fetch_drivers():
         series = 'GT World Challenge America'
         filtered = [entry for entry in data
                     if entry.get('series') == series]
-        drivers = []
+
+        entries_by_class = {}
+
+        # loop through filtered entries, and sort them into dictionary with keys as the class (pro, Pro/Am or am, etc) and the value as a list of entries
         for entry in filtered:
-            driver2 = entry.get('driver2')
-            drivers.append(entry['driver1'])
+            classification = entry['classification']
 
-            if driver2:
-                drivers.append(entry['driver2'])
+            if classification in entries_by_class:
+                entries_by_class[classification].append(entry)
+            else:
+                entries_by_class[classification] = [entry]
 
-        sorted_drivers = sorted(
-            drivers, key=lambda x: x['totalPoints'], reverse=True)
-        print(sorted_drivers)
+        # Loop through each list for Pro, Pro/Am etc, and alter the list, to be a list of drivers instead of a list of entries. then sort by most points
+        for classification in entries_by_class:
+            drivers = []
+            entry_list = entries_by_class[classification]
+            for entry in entry_list:
+                driver2 = entry.get('driver2')
+                drivers.append(entry['driver1'])
+
+                if driver2:
+                    drivers.append(entry['driver2'])
+
+            sorted_drivers = sorted(
+                drivers, key=lambda x: x['totalPoints'], reverse=True)
+
+            entries_by_class[classification] = sorted_drivers
+
+        print(json.dumps(entries_by_class, indent=4))
 
     else:
         return HTTPResponse('Failed to fetch data from API')
+
+
+if __name__ == "__main__":
+    fetch_drivers()
