a5ca4535fd04b62391ea0b73ae69d0f2ed1d055c
Brendan Woo <woo.brendan@gmail.com>
2023-06-27 12:44:02 -0700

    create points interface and team points interface

---
+++ express_mongo/models/team_points_schema.ts
@@ -0,0 +1,29 @@
+interface TeamPoints {
+  name: string;
+  classification: string;
+  series: string;
+  points: PointsInterface;
+}
+
+interface PointsInterface {
+  R1: number | null;
+  R2: number | null;
+  R3: number | null;
+  R4: number | null;
+  R5: number | null;
+  R6: number | null;
+  R7: number | null;
+  R8: number | null;
+  R9: number | null;
+  R10: number | null;
+  R11: number | null;
+  R12: number | null;
+  R13: number | null;
+  R14?: number | null;
+  R15?: number | null;
+  R16?: number | null;
+  R17?: number | null;
+  R18?: number | null;
+}
+
+// interface Team
