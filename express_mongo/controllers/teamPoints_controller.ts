61753fd32372443da9448d6580f5ad446532a69c
Brendan Woo <woo.brendan@gmail.com>
2023-06-28 16:50:57 -0700

    replace addteampoints controller with handleteampoints to add or update team points in array

--- express_mongo/controllers/teamPoints_controller.ts
+++ express_mongo/controllers/teamPoints_controller.ts
@@ -32,20 +32,54 @@ const getTeamPoints = async (req: Request, res: Response) => {
 };
 
 // For New entry that doesnt exist
-const addTeamToTeamPoints = async (req: Request, res: Response) => {
+// const addTeamToTeamPoints = async (req: Request, res: Response) => {
+//   const seriesName = req.params.seriesName;
+//   const { teamName, classification, points } = req.body;
+
+//   try {
+//     const series = await SeriesPoints.findOne({ name: seriesName });
+//     if (series) {
+//       const newTeam = {
+//         teamName,
+//         classification,
+//         points,
+//       };
+
+//       series.teamPoints.push(newTeam);
+//       await series.save();
+//       res.status(200).json({ newTeam });
+//     } else {
+//       return res.status(400).json({ message: "Series Not Found" });
+//     }
+//   } catch (error) {
+//     return res.status(500).json({ error });
+//   }
+// };
+
+const handleTeamPoints = async (req: Request, res: Response) => {
   const seriesName = req.params.seriesName;
   const { teamName, classification, points } = req.body;
 
   try {
     const series = await SeriesPoints.findOne({ name: seriesName });
+    let found = false;
     if (series) {
+      series.teamPoints.forEach((entry, index) => {
+        if (entry.teamName === teamName) {
+          series.teamPoints[index] = req.body;
+          found = true;
+        }
+      });
+
       const newTeam = {
         teamName,
         classification,
         points,
       };
 
-      series.teamPoints.push(newTeam);
+      if (!found) {
+        series.teamPoints.push(newTeam);
+      }
       await series.save();
       res.status(200).json({ newTeam });
     } else {
@@ -59,7 +93,7 @@ const addTeamToTeamPoints = async (req: Request, res: Response) => {
 export default {
   getBySeries,
   getTeamPoints,
-  addTeamToTeamPoints,
+  handleTeamPoints,
 };
 
 // TeamPoints {
