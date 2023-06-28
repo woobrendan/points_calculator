1b12d64c9093b836ffac5052f1a3f5bf441cff53
Brendan Woo <woo.brendan@gmail.com>
2023-06-28 15:21:50 -0700

    create controller for adding to a team to team points

--- express_mongo/controllers/teamPoints_controller.ts
+++ express_mongo/controllers/teamPoints_controller.ts
@@ -31,7 +31,39 @@ const getTeamPoints = async (req: Request, res: Response) => {
   }
 };
 
+// For New entry that doesnt exist
+const addTeamToTeamPoints = async (req: Request, res: Response) => {
+  const seriesName = req.params.seriesName;
+  const { teamName, classification, points } = req.body;
+
+  try {
+    const series = await SeriesPoints.findOne({ name: seriesName });
+    if (series) {
+      const newTeam = {
+        teamName,
+        classification,
+        points,
+      };
+
+      series.teamPoints.push(newTeam);
+      await series.save();
+      res.status(200).json({ newTeam });
+    } else {
+      return res.status(400).json({ message: "Series Not Found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
 export default {
   getBySeries,
   getTeamPoints,
+  addTeamToTeamPoints,
 };
+
+// TeamPoints {
+//     teamName: string;
+//     classification: string;
+//     points: PointsInterface;
+//   }
