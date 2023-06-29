f6946afa5442a1d18920c9dec75914a1f51487aa
Brendan Woo <woo.brendan@gmail.com>
2023-06-29 16:57:13 -0700

    replace addByTeam controller to be handleTeamPOints, and change url to just be req.param series

--- express_mongo/routes/teamPointsRoutes.ts
+++ express_mongo/routes/teamPointsRoutes.ts
@@ -4,7 +4,7 @@ import controller from "../controllers/teamPoints_controller";
 const router = express.Router();
 
 router.get("/:series/:pointsType", controller.getBySeries);
-router.post("/:series/teamPoints", controller.addTeamToTeamPoints);
+router.post("/:series", controller.handleTeamPoints);
 router.get("/:series", controller.getBySeries);
 
 export = router;
