09898a5ee97fe1ca313c01e43e870f1659a6f64e
Brendan Woo <woo.brendan@gmail.com>
2023-06-29 18:14:26 -0700

    create func setNewTeamPoints to handle logic for when a new team is added to team list. add round and points to params from req.bodsy inside handleTeam Points

--- express_mongo/controllers/teamPoints_controller.ts
+++ express_mongo/controllers/teamPoints_controller.ts
@@ -1,14 +1,46 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import SeriesPoints from "../models/Points/seriesPoints_schema";
+import { PointsInterface } from "../models/Points/points_models";
 
 const getSeriesName = (val: string) => {
-    if (val === "gtwca") return "GT World Challenge America"
-    if (val === "pgt4a") return "Pirelli GT4 America"
-    if (val === "gta") return "GT America"
-    if (val === "tca") return "TC America"
-    if (val === "tgr") return "Toyota GR Cup"
-}
+  if (val === "gtwca") return "GT World Challenge America";
+  if (val === "pgt4a") return "Pirelli GT4 America";
+  if (val === "gta") return "GT America";
+  if (val === "tca") return "TC America";
+  if (val === "tgr") return "Toyota GR Cup";
+};
+
+const setNewTeamPoints = (round: string, points: number) => {
+  const pts: PointsInterface = {
+    R1: null,
+    R2: null,
+    R3: null,
+    R4: null,
+    R5: null,
+    R6: null,
+    R7: null,
+    R8: null,
+    R9: null,
+    R10: null,
+    R11: null,
+    R12: null,
+    R13: null,
+    R14: null,
+    R15: null,
+    R16: null,
+    R17: null,
+    R18: null,
+  };
+
+  for (const roundNum in pts) {
+    if (roundNum === round) {
+      pts[roundNum] = points;
+    }
+  }
+
+  return pts;
+};
 
 const getAll = async (req: Request, res: Response) => {
   return SeriesPoints.find()
@@ -18,7 +50,7 @@ const getAll = async (req: Request, res: Response) => {
 
 const getBySeries = async (req: Request, res: Response) => {
   const series = req.params.series;
-  let seriesName = getSeriesName(series)
+  let seriesName = getSeriesName(series);
 
   try {
     const teamsBySeries = await SeriesPoints.findOne({ name: seriesName });
@@ -46,36 +78,11 @@ const getTeamPoints = async (req: Request, res: Response) => {
   }
 };
 
-// For New entry that doesnt exist
-// const addTeamToTeamPoints = async (req: Request, res: Response) => {
-//   const seriesName = req.params.seriesName;
-//   const { teamName, classification, points } = req.body;
-
-//   try {
-//     const series = await SeriesPoints.findOne({ name: seriesName });
-//     if (series) {
-//       const newTeam = {
-//         teamName,
-//         classification,
-//         points,
-//       };
-
-//       series.teamPoints.push(newTeam);
-//       await series.save();
-//       res.status(200).json({ newTeam });
-//     } else {
-//       return res.status(400).json({ message: "Series Not Found" });
-//     }
-//   } catch (error) {
-//     return res.status(500).json({ error });
-//   }
-// };
+//** RIGHT NOW WILL CHANGE PREVIOUS ROUND NULL VALUES NEED TO LOOP */
 
 const handleTeamPoints = async (req: Request, res: Response) => {
-  
-  const { teamName, classification, points } = req.body;
-  const seriesName = getSeriesName(req.params.series)
-  console.log('seriesName', seriesName)
+  const { teamName, classification, round, points } = req.body;
+  const seriesName = getSeriesName(req.params.series);
 
   try {
     const series = await SeriesPoints.findOne({ name: seriesName });
@@ -91,7 +98,7 @@ const handleTeamPoints = async (req: Request, res: Response) => {
       const newTeam = {
         teamName,
         classification,
-        points,
+        points: setNewTeamPoints(round, points),
       };
 
       if (!found) {
