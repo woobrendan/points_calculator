0ee7e4fc0d12872debc836f2157ffb75b39d346a
Brendan Woo <woo.brendan@gmail.com>
2023-06-30 13:58:00 -0700

    add route for team standing handler

--- points_calculator/standings/urls.py
+++ points_calculator/standings/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'standing'
 
 urlpatterns = [
-    path('drivers/', views.drivers_standing, name="drivers")
+    path('drivers/', views.drivers_standing, name="drivers"),
+    path('team/', views.team_standing, name="team")
 ]
