088cf5a7704bc1fb84d18a2065c6f9c804c16e38
Brendan Woo <woo.brendan@gmail.com>
2023-06-30 13:58:15 -0700

    Create controller for team standing

--- points_calculator/standings/views.py
+++ points_calculator/standings/views.py
@@ -10,13 +10,17 @@ def drivers_standing(request):
     # if the value is a tuple, means fetch failed, and we have status code and none value
     if isinstance(drivers, tuple):
         status_code, data = drivers
-        error_message = f"Failed to fetch data. Status code: {status_code}"
+        error_message = f"Failed to fetch data. Status code: { status_code }"
         return jsonify({"error": error_message}), status_code
 
     else:
         return render(request, 'standing/drivers_standing.html', {
             'drivers': drivers
         })
+
+
+def team_standing(request):
+    return render(request, 'standing/team_standing.html')
 
 
 if __name__ == "__main__":
