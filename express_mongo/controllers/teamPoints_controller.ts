7210e605c0bde8fd9d31f817d83447b81e492254
Brendan Woo <woo.brendan@gmail.com>
2023-06-30 18:27:56 -0700

    update handleTeamPoints for whena team is found, to use updateTeamPoints to update current round points

--- express_mongo/controllers/teamPoints_controller.ts
+++ express_mongo/controllers/teamPoints_controller.ts
@@ -1,46 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import SeriesPoints from "../models/Points/seriesPoints_schema";
-import { PointsInterface } from "../models/Points/points_models";
-
-const getSeriesName = (val: string) => {
-  if (val === "gtwca") return "GT World Challenge America";
-  if (val === "pgt4a") return "Pirelli GT4 America";
-  if (val === "gta") return "GT America";
-  if (val === "tca") return "TC America";
-  if (val === "tgr") return "Toyota GR Cup";
-};
-
-const setNewTeamPoints = (round: string, points: number) => {
-  const pts: PointsInterface = {
-    R1: null,
-    R2: null,
-    R3: null,
-    R4: null,
-    R5: null,
-    R6: null,
-    R7: null,
-    R8: null,
-    R9: null,
-    R10: null,
-    R11: null,
-    R12: null,
-    R13: null,
-    R14: null,
-    R15: null,
-    R16: null,
-    R17: null,
-    R18: null,
-  };
-
-  for (const roundNum in pts) {
-    if (roundNum === round) {
-      pts[roundNum] = points;
-    }
-  }
-
-  return pts;
-};
+import {
+  setNewTeamPoints,
+  getSeriesName,
+  updateTeamPoints,
+} from "../functions/teamPointsHelper";
 
 const getAll = async (req: Request, res: Response) => {
   return SeriesPoints.find()
@@ -90,7 +55,10 @@ const handleTeamPoints = async (req: Request, res: Response) => {
     if (series) {
       series.teamPoints.forEach((entry, index) => {
         if (entry.teamName === teamName) {
-          series.teamPoints[index] = req.body;
+          series.teamPoints[index] = {
+            ...entry,
+            points: updateTeamPoints(round, points, entry.points),
+          };
           found = true;
         }
       });
