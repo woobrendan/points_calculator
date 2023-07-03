48117c2a057a771df031d99f801bad7f9e798c1a
Brendan Woo <woo.brendan@gmail.com>
2023-07-03 15:10:30 -0700

    add param series to drivers url

--- points_calculator/standings/urls.py
+++ points_calculator/standings/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'standing'
 
 urlpatterns = [
-    path('drivers/', views.drivers_standing, name="drivers"),
+    path('drivers/<str:series>/', views.drivers_standing, name="drivers"),
     path('team/<str:series>/', views.team_standing, name="team")
 ]
