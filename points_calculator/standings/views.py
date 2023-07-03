cfd0e13897fd4ce8acf03652ec4023b6aba33317
Brendan Woo <woo.brendan@gmail.com>
2023-07-03 14:59:29 -0700

    pass series param to drivers_standing. remove logic to create entries by class and move into helper func. create team stnading controller using fetch_team_standing

--- points_calculator/standings/views.py
+++ points_calculator/standings/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from flask import jsonify
 
-from .functions import fetch_drivers
+from .functions import fetch_drivers, fetch_team_standing
 
 
-def drivers_standing(request):
-    drivers = fetch_drivers.fetch_drivers()
+def drivers_standing(request, series):
+    drivers = fetch_drivers.fetch_drivers(series)
 
     # if the value is a tuple, means fetch failed, and we have status code and none value
     if isinstance(drivers, tuple):
@@ -19,8 +19,19 @@ def drivers_standing(request):
         })
 
 
-def team_standing(request):
-    return render(request, 'standing/team_standing.html')
+def team_standing(request, series):
+    team_standing = fetch_team_standing.fetch_team_standings(series)
+
+    # if the value is a tuple, means fetch failed, and we have status code and none value
+    if isinstance(team_standing, tuple):
+        status_code, data = team_standing
+        error_message = f"Failed to fetch data. Status code: { status_code }"
+        return jsonify({"error": error_message}), status_code
+
+    else:
+        return render(request, 'standing/team_standing.html', {
+            'team_standing': team_standing
+        })
 
 
 if __name__ == "__main__":
