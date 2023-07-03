3e5c95eb3b6a8b213faf836156fd738c7837e0f6
Brendan Woo <woo.brendan@gmail.com>
2023-07-03 15:31:51 -0700

    fix import statement for helpers and change status code from 201 to 200 for if block:

--- points_calculator/standings/functions/fetch_team_standing.py
+++ points_calculator/standings/functions/fetch_team_standing.py
@@ -1,12 +1,12 @@
 import requests
-from . import helpers
+import helpers
 
 
 def fetch_team_standings(series):
     url = f'http://localhost:2020/teamPoints/{series}'
     response = requests.get(url)
 
-    if response.status_code == 201:
+    if response.status_code == 200:
         data = response.json()['teamsBySeries']
         team_points_list = data['teamPoints']
 
