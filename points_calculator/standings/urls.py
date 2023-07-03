708fb5be27c575b09b4bb4a1b7ce49757ac22e65
Brendan Woo <woo.brendan@gmail.com>
2023-07-03 14:46:43 -0700

    add series req param to team url

--- points_calculator/standings/urls.py
+++ points_calculator/standings/urls.py
@@ -6,5 +6,5 @@ app_name = 'standing'
 
 urlpatterns = [
     path('drivers/', views.drivers_standing, name="drivers"),
-    path('team/', views.team_standing, name="team")
+    path('team/<str:series>/', views.team_standing, name="team")
 ]
