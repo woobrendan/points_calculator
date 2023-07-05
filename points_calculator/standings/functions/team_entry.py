b273e3f1f9e90bda5482de3331dfa9204c4b377f
Brendan Woo <woo.brendan@gmail.com>
2023-07-05 14:01:34 -0700

    fixx import statement, fix spelling on classification. change init value to be points_dict and to get total points from the dict values

--- points_calculator/standings/functions/team_entry.py
+++ points_calculator/standings/functions/team_entry.py
@@ -1,12 +1,13 @@
-from points import Points
+from . import points
 
 
 class TeamEntry:
-    def __init__(self, name: str, classification: str, points_dict: dict, type):
+    def __init__(self, name: str, classification: str, points_dict: dict):
         self.name = name
-        self.classifcation = classification
-        self.points = Points(points_dict)
-        self.total_points = sum(value or 0 for value in self.points.values())
+        self.classification = classification
+        self.points = points.Points(points_dict)
+        self.total_points = sum(
+            value or 0 for value in self.points.__dict__.values())
 
         for round_num in self.points.__dict__:
             if getattr(self.points, round_num) is None:
