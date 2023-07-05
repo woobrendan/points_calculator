433a6bbb48e53c228e12bb11672a543fc3c6028a
Brendan Woo <woo.brendan@gmail.com>
2023-07-05 11:52:43 -0700

    create class TeamEntry, and move some logic from classEntries into TeamEntry

--- points_calculator/standings/functions/helpers.py
+++ points_calculator/standings/functions/helpers.py
@@ -1,23 +1,31 @@
+class TeamEntry:
+    def __init__(self, name: str, classification: str, points, type):
+        self.name = name
+        self.classifcation = classification
+        self.points = points
+        self.total_points = sum(value or 0 for value in points.values())
+
+        for round_num in points:
+            if points[round_num] is None:
+                points[round_num] = ''
+
+
 def classEntries(entry_list):
     entries_by_class = {}
 
     # loop through filtered entries, and sort them into dictionary with keys as the class (pro, Pro/Am or am, etc) and the value as a list of entries
     for entry in entry_list:
         classification = entry['classification']
-        entry['totalPoints'] = sum(
-            value or 0 for value in entry['points'].values())
-
-        for roundNum in entry['points']:
-            if entry['points'][roundNum] is None:
-                entry['points'][roundNum] = ''
+        points = entry['points']
+        entry_obj = TeamEntry(entry['teamName'], classification, points)
 
         if classification in entries_by_class:
-            entries_by_class[classification].append(entry)
+            entries_by_class[classification].append(entry_obj)
         else:
-            entries_by_class[classification] = [entry]
+            entries_by_class[classification] = [entry_obj]
 
     for class_list in entries_by_class.values():
-        class_list.sort(key=lambda x: x['totalPoints'], reverse=True)
+        class_list.sort(key=lambda x: x.totalPoints, reverse=True)
 
     return entries_by_class
 
