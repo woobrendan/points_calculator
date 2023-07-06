bbc67bdfe374d0b66d8e1f301c0efe4e86ea8571
Brendan Woo <woo.brendan@gmail.com>
2023-07-06 11:40:12 -0700

    import helpers and inside team__standing, use getRounds for variable round_list to be passed to front end to render thead for rounds

--- points_calculator/standings/views.py
+++ points_calculator/standings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from flask import jsonify
 
-from .functions import fetch_drivers, fetch_team_standing
+from .functions import fetch_drivers, fetch_team_standing, helpers
 
 
 def drivers_standing(request, series):
@@ -23,8 +23,10 @@ def team_standing(request, series):
     team_standing = fetch_team_standing.fetch_team_standings(series)
 
     if series != 'gta':
-        for classification, lists in team_standing.items():
+        for _, lists in team_standing.items():
             keys_to_remove = ['R14', 'R15', 'R16', 'R17', 'R18']
+
+            # TC America & GT4 America have 14 rounds
             if series == 'tca' or series == 'pgt4a':
                 del keys_to_remove[0]
 
@@ -35,6 +37,9 @@ def team_standing(request, series):
                     if key in points.__dict__:
                         delattr(points, key)
 
+    _, team_list = list(team_standing.items())[0]
+    round_list = helpers.getRounds(team_list[0].points)
+
     # if the value is a tuple, means fetch failed, and we have status code and none value
     if isinstance(team_standing, tuple):
         status_code, data = team_standing
@@ -43,9 +48,6 @@ def team_standing(request, series):
 
     else:
         return render(request, 'standing/team_standing.html', {
-            'team_standing': team_standing
+            'team_standing': team_standing,
+            'round_list': round_list
         })
-
-
-if __name__ == "__main__":
-    drivers_standing()
