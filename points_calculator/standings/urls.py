813c6877c125bd9c13be48e1424c3dbfdee93e26
Brendan Woo <woo.brendan@gmail.com>
2023-07-07 12:03:38 -0700

    create route handler for stnading new

--- points_calculator/standings/urls.py
+++ points_calculator/standings/urls.py
@@ -6,5 +6,6 @@ app_name = 'standing'
 
 urlpatterns = [
     path('drivers/<str:series>/', views.drivers_standing, name="drivers"),
-    path('team/<str:series>/', views.team_standing, name="team")
+    path('team/<str:series>/', views.team_standing, name="team"),
+    path('new/', views.new_result, name="new")
 ]
