50c1a036fe87054742d398fe533c6725a8673631
Brendan Woo <woo.brendan@gmail.com>
2023-07-10 14:29:19 -0700

    create fun team_results_byClass and create logic to create dict for each class and sort by poiubnts

--- points_calculator/standings/functions/helpers.py
+++ points_calculator/standings/functions/helpers.py
@@ -39,3 +39,21 @@ def getRounds(points_dict):
     for roundNum in points_dict.__dict__:
         round_list.append(roundNum)
     return round_list
+
+
+def team_results_byClass(entry_list):
+    entries_by_class = {}
+
+    # loop through filtered entries, and sort them into dictionary with keys as the class (pro, Pro/Am or am, etc) and the value as a list of entries
+    for entry in entry_list:
+        classification = entry['Class']
+
+        if classification in entries_by_class:
+            entries_by_class[classification].append(entry)
+        else:
+            entries_by_class[classification] = [entry]
+
+    for class_list in entries_by_class.values():
+        class_list.sort(key=lambda x: int(x['Points']), reverse=True)
+
+    return entries_by_class
