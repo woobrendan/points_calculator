f1146dd021fcd2464c10c48375c1241aca3c864c
Brendan Woo <woo.brendan@gmail.com>
2023-07-11 16:14:15 -0700

    add logic to handle if the manuf entry is not within the db list and to push the new manuf entry

--- express_mongo/functions/manufPtsHelper.ts
+++ express_mongo/functions/manufPtsHelper.ts
@@ -1,45 +1,67 @@
-import { ReqPointsArr, ManufacturerPoints } from "../models/Points/points_models";
+import {
+    ReqPointsArr,
+    ManufacturerPoints,
+} from "../models/Points/points_models";
 import SeriesPoints from "../models/Points/seriesPoints_schema";
+import { setNewTeamPoints } from "./teamPointsHelper";
 
-const handleManufPoints = async (manufObj: ReqPointsArr, seriesName: string, round: string) => {
+const handleManufPoints = async (
+    manufObj: ReqPointsArr,
+    seriesName: string,
+    round: string,
+) => {
+    try {
+        const series = await SeriesPoints.findOne({ name: seriesName });
 
-  try {
-    const series = await SeriesPoints.findOne({ name: seriesName });
+        if (series) {
+            const manufList = series.manufPoints;
 
-    if (series) {
-        const manufList = series.manufPoints;
+            // Loop through keys (class) from the incoming array and check if those keys exist inside the db for manufPoints
+            for (const classification in manufObj) {
+                if (classification in manufList) {
+                    // Once found, declare variables for each backend and incoming array for the appropriate class
+                    const dbArr = manufList[classification];
+                    const reqArr = manufObj[classification];
 
-        // Loop through the incoming array of results, and loop through the backend manuf Obj to match classes
-        for (const classification in manufObj) {
-            if (classification in manufList) {
-                const dbArr = manufList[classification];
-                const reqArr = manufObj[classification];
+                    for (const newResult of reqArr) {
+                        const { Manufacturer, Points } = newResult;
+                        let found = false;
 
-                for (const newResult of reqArr) {
-                    const { Manufacturer, Points } = newResult
+                        for (const dbEntry of dbArr) {
+                            if (dbEntry.manufName === Manufacturer) {
+                                dbEntry.points[round] = Points;
+                                found = true;
+                                break;
+                            }
+                        }
 
-                    for (const dbEntry of dbArr) {
-                        if (dbEntry.manufName === Manufacturer) {
-                            dbEntry.points[round] = Points
-                        } 
+                        if (!found) {
+                            const newManuf: ManufacturerPoints = {
+                                manufName: Manufacturer,
+                                classification: newResult.Class,
+                                points: setNewTeamPoints(round, Points),
+                            };
+
+                            dbArr.push(newManuf);
+
+                            await series.save();
+                        }
                     }
                 }
             }
         }
-    }
-  } catch (error) {}
+    } catch (error) {}
 };
 
 //REQUEST || manufObj
 // {'Pro': [{'Pos': '1', 'PIC': '1', '#': '93', 'Class': 'Pro', 'Points': 25, 'Team': 'Racers Edge Motorsports', 'Vehicle': 'Acura NSX GT3 EVO22', 'Series': 'gtwca', 'Manufacturer': 'Acura'}, {'Pos': '2', 'PIC': '2', '#': '28', 'Class': 'Pro', 'Points': 18, 'Team': 'RS1', 'Vehicle': 'Porsche GT3 R 992', 'Series': 'gtwca', 'Manufacturer': 'Porsche'}, {'Pos': '13', 'PIC': '4', '#': '94', 'Class': 'Pro', 'Points': 15, 'Team': 'BimmerWorld', 'Vehicle': 'BMW M4 GT3', 'Series': 'gtwca', 'Manufacturer': 'BMW'}], 'Pro-Am': [{'Pos': '3', 'PIC': '1', '#': '120', 'Class': 'Pro-Am', 'Points': 25, 'Team': 'Wright Motorsports', 'Vehicle': 'Porsche 911 GT3-R (991.ii)', 'Series': 'gtwca', 'Manufacturer': 'Porsche'}, {'Pos': '4', 'PIC': '2', '#': '007', 'Class': 'Pro-Am', 'Points': 18, 'Team': 'TRG - The Racers Group', 'Vehicle': 'Aston Martin Vantage AMR GT3', 'Series': 'gtwca', 'Manufacturer': 'Aston Martin'}, {'Pos': '7', 'PIC': '4', '#': '91', 'Class': 'Pro-Am', 'Points': 15, 'Team': 'DXDT Racing', 'Vehicle': 'Mercedes-AMG GT3', 'Series': 'gtwca', 'Manufacturer': 'Mercedes AMG GmbH'}, {'Pos': '8', 'PIC': '5', '#': '38', 'Class': 'Pro-Am', 'Points': 12, 'Team': 'ST Racing', 'Vehicle': 'BMW M4 GT3', 'Series': 'gtwca', 'Manufacturer': 'BMW'}, {'Pos': '9', 'PIC': '6', '#': '33', 'Class': 'Pro-Am', 'Points': 10, 'Team': 'Triarsi Competizione', 'Vehicle': 'Ferrari 296 GT3', 'Series': 'gtwca', 'Manufacturer': 'Ferrari'}], 'Am': [{'Pos': '15', 'PIC': '1', '#': '43', 'Class': 'Am', 'Points': 25, 'Team': 'RealTime Racing', 'Vehicle': 'Mercedes-AMG GT3', 'Series': 'gtwca', 'Manufacturer': 'Mercedes AMG GmbH'}]}
-
 
 // teamsBySeries: {
 //     _id: "64a883d00c5298fa6cfe74fa",
 //     name: "GT World Challenge",
 //     teamPoints: [
 //     ],
-//     manufPoints: { 
+//     manufPoints: {
 //     Pro: [
 //         points: {
 //         R1: 18,
@@ -64,11 +86,5 @@ const handleManufPoints = async (manufObj: ReqPointsArr, seriesName: string, rou
 //         manufName: "Acura",
 //         classification: "Pro",
 //         _id: "64ac4e24d8ba27b76d327875"
-//         ] 
+//         ]
 // },
-
-// else {
-//     const newManuf = {
-//         manufName
-//     }
-// }
