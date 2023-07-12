c65e18b9ff101f57c8d73aa55a7b0b99503d845a
Brendan Woo <woo.brendan@gmail.com>
2023-07-12 15:31:53 -0700

    create func handle__rounds to handle round allocation for views

--- points_calculator/standings/functions/helpers.py
+++ points_calculator/standings/functions/helpers.py
@@ -39,3 +39,22 @@ def getRounds(points_dict):
     for roundNum in points_dict.__dict__:
         round_list.append(roundNum)
     return round_list
+
+
+def handle_rounds(series, class_entries):
+    if series != 'gta':
+        for _, lists in class_entries.items():
+            keys_to_remove = ['R14', 'R15', 'R16', 'R17', 'R18']
+
+            # TC America & GT4 America have 14 rounds
+            if series == 'tca' or series == 'pgt4a':
+                del keys_to_remove[0]
+
+            # List of TeamEntry
+            for team in lists:
+                points = team.points
+                for key in keys_to_remove:
+                    if key in points.__dict__:
+                        delattr(points, key)
+
+    return class_entries
