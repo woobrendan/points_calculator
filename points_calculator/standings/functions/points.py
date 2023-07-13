9418a576112e127ec9fbd1e0a5bed998be41cd9c
Brendan Woo <woo.brendan@gmail.com>
2023-07-13 16:00:37 -0700

    move update_team_points into points file from team_points, change name to just update points

--- points_calculator/standings/functions/points.py
+++ points_calculator/standings/functions/points.py
@@ -2,3 +2,18 @@ class Points:
     def __init__(self, points_dict):
         for round_num, value in points_dict.items():
             setattr(self, round_num, value)
+
+
+def update_points(arr):
+    points = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
+
+    for i, entry in enumerate(arr):
+        if i < 10:
+            if entry['PIC'] == '':
+                entry['Points'] = 0
+            else:
+                entry['Points'] = points[i]
+        else:
+            entry['Points'] = 0
+
+    return arr
