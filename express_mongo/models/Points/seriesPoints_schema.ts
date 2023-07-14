0ed85cac8d4ccdf08741c15a1e4231347a996d09
Brendan Woo <woo.brendan@gmail.com>
2023-07-14 13:06:27 -0700

    update manufPoints to be an object, type map for an array of manufpts interface. change name key to be seriesName

--- express_mongo/models/Points/seriesPoints_schema.ts
+++ express_mongo/models/Points/seriesPoints_schema.ts
@@ -1,10 +1,10 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { Series } from "./points_models";
+import { Series, TeamPoints, IManufPoints } from "./points_models";
 
 export interface SeriesPointsModel extends Series, Document {}
 
 const seriesPointsSchema: Schema = new Schema({
-  name: { type: String, required: true },
+  seriesName: { type: String, required: true },
   teamPoints: [
     {
       teamName: { type: String },
@@ -29,33 +29,22 @@ const seriesPointsSchema: Schema = new Schema({
         R17: { type: Schema.Types.Mixed },
         R18: { type: Schema.Types.Mixed },
       },
+      default: {},
     },
   ],
-  manufPoints: [
-    {
-      manufName: { type: String, required: true },
-      points: {
-        R1: { type: Schema.Types.Mixed },
-        R2: { type: Schema.Types.Mixed },
-        R3: { type: Schema.Types.Mixed },
-        R4: { type: Schema.Types.Mixed },
-        R5: { type: Schema.Types.Mixed },
-        R6: { type: Schema.Types.Mixed },
-        R7: { type: Schema.Types.Mixed },
-        R8: { type: Schema.Types.Mixed },
-        R9: { type: Schema.Types.Mixed },
-        R10: { type: Schema.Types.Mixed },
-        R11: { type: Schema.Types.Mixed },
-        R12: { type: Schema.Types.Mixed },
-        R13: { type: Schema.Types.Mixed },
-        R14: { type: Schema.Types.Mixed },
-        R15: { type: Schema.Types.Mixed },
-        R16: { type: Schema.Types.Mixed },
-        R17: { type: Schema.Types.Mixed },
-        R18: { type: Schema.Types.Mixed },
+  manufPoints: {
+    type: Map,
+    of: [
+      {
+        manufName: { type: String, required: true },
+        points: {
+          type: Map,
+          of: Schema.Types.Mixed,
+        },
       },
-    },
-  ],
+    ],
+    default: new Map(),
+  },
 });
 
 export default mongoose.model<SeriesPointsModel>(
