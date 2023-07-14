15d1d0466f8a05241803435603855efd3151b4dd
Brendan Woo <woo.brendan@gmail.com>
2023-07-14 12:00:36 -0700

    set up template for handle points for manuf

---
+++ express_mongo/functions/manufPtsHelper.ts
@@ -0,0 +1,9 @@
+import { ReqPoints } from "../models/Points/points_models";
+
+const handleManufPoints = async (manufObj: ReqPoints[]) => {
+  for (const classifcation in manufObj) {
+  }
+
+  try {
+  } catch (error) {}
+};
