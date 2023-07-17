42057dfd847ab184aca1301ea48a28cc70a6f111
Brendan Woo <woo.brendan@gmail.com>
2023-07-17 16:47:49 -0700

    add new interrface for ITeamsPpoints for array of points arr interface. add comments for readability

--- express_mongo/models/Points/points_models.ts
+++ express_mongo/models/Points/points_models.ts
@@ -1,59 +1,66 @@
+export interface PointsInterface {
+    [key: string]: number | null;
+    R1: number | null;
+    R2: number | null;
+    R3: number | null;
+    R4: number | null;
+    R5: number | null;
+    R6: number | null;
+    R7: number | null;
+    R8: number | null;
+    R9: number | null;
+    R10: number | null;
+    R11: number | null;
+    R12: number | null;
+    R13: number | null;
+    R14: number | null;
+    R15: number | null;
+    R16: number | null;
+    R17: number | null;
+    R18: number | null;
+}
+
+//** Interfaces for Team and Manuf Array */
 export interface TeamPoints {
-  teamName: string;
-  classification: string;
-  points: PointsInterface;
+    teamName: string;
+    classification: string;
+    points: PointsInterface;
+}
+
+interface ITeamPoints {
+    [key: string]: TeamPoints[];
 }
 
 export interface ManufacturerPoints {
-  manufName: string;
-  classification: string;
-  points: PointsInterface;
+    manufName: string;
+    classification: string;
+    points: PointsInterface;
 }
 
 export interface IManufPoints {
-  [key: string]: ManufacturerPoints[];
+    [key: string]: ManufacturerPoints[];
 }
 
-export interface PointsInterface {
-  [key: string]: number | null;
-  R1: number | null;
-  R2: number | null;
-  R3: number | null;
-  R4: number | null;
-  R5: number | null;
-  R6: number | null;
-  R7: number | null;
-  R8: number | null;
-  R9: number | null;
-  R10: number | null;
-  R11: number | null;
-  R12: number | null;
-  R13: number | null;
-  R14: number | null;
-  R15: number | null;
-  R16: number | null;
-  R17: number | null;
-  R18: number | null;
-}
-
+//** Interface for Series Points Schema */
 export interface Series {
-  seriesName: string;
-  teamPoints: TeamPoints[];
-  manufPoints: IManufPoints;
+    seriesName: string;
+    teamPoints: ITeamPoints;
+    manufPoints: IManufPoints;
 }
 
+//** Interfaces for points coming from front end */
 export interface ReqPoints {
-  [key: string]: string | number;
-  Pos: string;
-  PIC: string;
-  Class: string;
-  Points: number;
-  Team: string;
-  Vehicle: string;
-  Series: string;
-  Manufacturer: string;
+    [key: string]: string | number;
+    Pos: string;
+    PIC: string;
+    Class: string;
+    Points: number;
+    Team: string;
+    Vehicle: string;
+    Series: string;
+    Manufacturer: string;
 }
 
 export interface ReqPointsArr {
-  [key: string]: ReqPoints[];
+    [key: string]: ReqPoints[];
 }
