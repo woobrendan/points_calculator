2aa696406d613879c9a056d1d48a666ab149e6e2
Brendan Woo <woo.brendan@gmail.com>
2023-07-19 15:36:13 -0600

    import handler for gt3 and gt4. add if block to handle logic basedf on series

--- express_mongo/controllers/resultsController.ts
+++ express_mongo/controllers/resultsController.ts
@@ -2,17 +2,23 @@ import { Request, Response } from "express";
 import SeriesPoints from "../models/Points/seriesPoints_schema";
 import { getSeriesName } from "../functions/teamPointsHelper";
 import handleManufPoints from "../functions/manufPtsHelper";
+import handleGT3GT4ManufPts from "../functions/manufPtsHelper";
 
 const handleTeamManufPoints = async (req: Request, res: Response) => {
     const seriesName = getSeriesName(req.params.series);
     const { manufResults, teamResults, roundNum } = req.body;
 
     try {
-        const manuf = await handleManufPoints(
-            manufResults,
-            seriesName,
-            roundNum,
-        );
+        let manuf = false;
+        if (seriesName === "gtwca" || seriesName === "pgt4a") {
+            manuf = await handleGT3GT4ManufPts(
+                manufResults,
+                seriesName,
+                roundNum,
+            );
+        } else {
+            manuf = await handleManufPoints(manufResults, seriesName, roundNum);
+        }
         if (manuf) {
             return res.status(200).send();
         } else {
