cbe70815bcf60fc6a479dcfe305e7cbfb0bc5902
Brendan Woo <woo.brendan@gmail.com>
2023-07-25 17:15:21 -0400

    create class for manuf entries

---
+++ points_calculator/standings/functions/manuf/manuf_entry.py
@@ -0,0 +1,16 @@
+from .. import points
+
+
+class ManufEntry:
+    def __init__(self, name: str, classification: str, points_dict: dict):
+        self.name = name
+        self.classification = classification
+        self.points = points.Points(points_dict)
+        self.total_points = sum(
+            value or 0 for value in self.points.__dict__.values())
+
+        for round_num in self.points.__dict__:
+            if getattr(self.points, round_num) == 0:
+                setattr(self.points, round_num, 0)
+            elif getattr(self.points, round_num) is None:
+                setattr(self.points, round_num, '')
