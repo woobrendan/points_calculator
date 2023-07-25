c37a18561de8911e76911398ec20265b9d77bee6
Brendan Woo <woo.brendan@gmail.com>
2023-07-25 17:46:18 -0400

    adjust teamPoints schema to not be an array of obbjects and isntead be an object with dynamic keys and values

--- express_mongo/models/Points/seriesPoints_schema.ts
+++ express_mongo/models/Points/seriesPoints_schema.ts
@@ -5,33 +5,10 @@ export interface SeriesPointsModel extends Series, Document {}
 
 const seriesPointsSchema: Schema = new Schema({
     name: { type: String },
-    teamPoints: [
-        {
-            teamName: { type: String },
-            classification: { type: String },
-            points: {
-                R1: { type: Schema.Types.Mixed },
-                R2: { type: Schema.Types.Mixed },
-                R3: { type: Schema.Types.Mixed },
-                R4: { type: Schema.Types.Mixed },
-                R5: { type: Schema.Types.Mixed },
-                R6: { type: Schema.Types.Mixed },
-                R7: { type: Schema.Types.Mixed },
-                R8: { type: Schema.Types.Mixed },
-                R9: { type: Schema.Types.Mixed },
-                R10: { type: Schema.Types.Mixed },
-                R11: { type: Schema.Types.Mixed },
-                R12: { type: Schema.Types.Mixed },
-                R13: { type: Schema.Types.Mixed },
-                R14: { type: Schema.Types.Mixed },
-                R15: { type: Schema.Types.Mixed },
-                R16: { type: Schema.Types.Mixed },
-                R17: { type: Schema.Types.Mixed },
-                R18: { type: Schema.Types.Mixed },
-            },
-            default: {},
-        },
-    ],
+    teamPoints: {
+        type: Schema.Types.Mixed,
+        strict: false,
+    },
     manufPoints: {
         type: Schema.Types.Mixed,
     },
