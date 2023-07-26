4745b1c98851de729e6b272de20640ee2443f20d
Brendan Woo <woo.brendan@gmail.com>
2023-07-26 17:59:45 -0400

    remove list indexing when accessing data

--- points_calculator/standings/functions/fetch_team_standing.py
+++ points_calculator/standings/functions/fetch_team_standing.py
@@ -7,8 +7,7 @@ def fetch_team_standings(series):
     response = requests.get(url)
 
     if response.status_code == 200:
-        data = response.json()['teamsBySeries']['teamPoints'][0]
-        data.pop('_id')
+        data = response.json()['teamsBySeries']['teamPoints']
 
         return helpers.classEntries(data)
     else:
