a98d72284f9ee554a4a373eff93b74a7b72afc14
Brendan Woo <woo.brendan@gmail.com>
2023-07-26 16:42:09 -0400

    create route handler for manuf points

--- points_calculator/standings/urls.py
+++ points_calculator/standings/urls.py
@@ -7,5 +7,6 @@ app_name = 'standing'
 urlpatterns = [
     path('drivers/<str:series>/', views.drivers_standing, name="drivers"),
     path('team/<str:series>/', views.team_standing, name="team"),
+    path('manufacturer/<str:series>/', views.manuf_standing, name="manufacturer"),
     path('new/', views.new_result, name="new")
 ]
