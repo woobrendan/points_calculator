11638919ad6c93952f09f94d3bc94c296eec3bbd
Brendan Woo <woo.brendan@gmail.com>
2023-07-27 17:40:03 -0400

    create variable for gt3_gt4 based on series, pass to data fetching. create if block to later handle logic of data manipulation based on series

--- points_calculator/standings/functions/manuf/fetch_manuf_points.py
+++ points_calculator/standings/functions/manuf/fetch_manuf_points.py
@@ -1,16 +1,22 @@
+from ast import Gt
 import requests
-from .. import helpers
+from ..helpers import classEntries
 
 
 def fetch_team_standings(series):
     url = f'http://localhost:2020/teamPoints/{series}'
     response = requests.get(url)
 
+    GT3_GT4 = True if series == 'gtwca' or series == 'pgt4a' else False
+
     if response.status_code == 200:
-        manuf_val = 'manufPointsList' if series == 'gtwca' or series == 'pgt4a' else 'manufPoints'
+        manuf_val = 'manufPointsList' if GT3_GT4 else 'manufPoints'
 
-        data = response.json()['teamsBySeries']['teamPoints']
+        data = response.json()['teamsBySeries'][manuf_val]
 
-        return helpers.classEntries(data)
+        if GT3_GT4:
+            pass
+        else:
+            return classEntries(data)
     else:
         return (response.status_code, None)
