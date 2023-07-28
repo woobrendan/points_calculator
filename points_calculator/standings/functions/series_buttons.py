84d59ec1f3a26292306e17fa85c4ac29745f3bef
Brendan Woo <woo.brendan@gmail.com>
2023-07-28 13:11:20 -0400

    create loop logic to get series and add the value as the style, change button style to selected style

--- points_calculator/standings/functions/series_buttons.py
+++ points_calculator/standings/functions/series_buttons.py
@@ -1,6 +1,6 @@
 series_list = ['gtwca', 'pgt4a', 'gta', 'tca', 'grc']
 button_style = "px-4 py-2 text-sm font-medium text-white bg-red-600 border border-gray-200 rounded-lg hover:bg-black hover:text-red-400 hover:border-red-500"
-selected_style = "px-4 py-2 text-sm font-medium text-white bg-red-600 border border-gray-200 rounded-lg hover:bg-black hover:text-red-400 hover:border-red-500"
+selected_style = "px-4 py-2 text-sm font-medium text-white bg-black-600 border border-red-500 rounded-lg hover:bg-black hover:text-red-400 hover:border-red-500"
 
 
 def get_series_buttons(points_type, selected_series):
@@ -9,7 +9,9 @@ def get_series_buttons(points_type, selected_series):
         series_list.pop()
 
     seriesObj = {}
+
     for series in series_list:
-        seriesObj[series] = {
-            'name': series,
-        }
+        selected = selected_style if series == selected_series else button_style
+        seriesObj[series] = selected
+
+    return seriesObj
