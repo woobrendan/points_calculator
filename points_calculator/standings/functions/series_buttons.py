b5a6b7a5ebeadc552576c226aeca1657089f7ff2
Brendan Woo <woo.brendan@gmail.com>
2023-07-31 14:46:43 -0400

    update selected button styling to be background black. update logic to remove grc instead of pop from list

--- points_calculator/standings/functions/series_buttons.py
+++ points_calculator/standings/functions/series_buttons.py
@@ -1,12 +1,12 @@
 series_list = ['gtwca', 'pgt4a', 'gta', 'tca', 'grc']
 button_style = "px-4 py-2 text-sm font-medium text-white bg-red-600 border border-gray-200 rounded-lg hover:bg-black hover:text-red-400 hover:border-red-500"
-selected_style = "px-4 py-2 text-sm font-medium text-white bg-black-600 border border-red-500 rounded-lg hover:bg-black hover:text-red-400 hover:border-red-500"
+selected_style = "px-4 py-2 text-sm font-medium text-white bg-black-500 border border-red-500 rounded-lg hover:bg-black hover:text-red-400 hover:border-red-500"
 
 
 def get_series_buttons(points_type, selected_series):
     # GR Cup won't have manuf point standing
-    if points_type == 'manuf':
-        series_list.pop()
+    if points_type == 'manuf' and 'grc' in series_list:
+        series_list.remove('grc')
 
     seriesObj = {}
 
