644c22fec14d295c5c105dedac9a82f45658e659
Brendan Woo <woo.brendan@gmail.com>
2023-08-01 15:56:12 -0400

    remove series route from previous project

--- express_mongo/routes/seriesRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import controller from "../controllers/series_controller";
-
-const router = express.Router();
-
-router.get("/", controller.getAllSeries);
-
-export = router;
