61117ced5b2623c6a329df323e9210d57388093c
Brendan Woo <woo.brendan@gmail.com>
2023-08-02 16:39:21 -0400

    create variable for manuf_type to return the type of manufPoints wanted to be returned based on series, update seriesData json call

--- express_mongo/controllers/manufPoints_controller.ts
+++ express_mongo/controllers/manufPoints_controller.ts
@@ -13,10 +13,15 @@ const getManufPoints = async (req: Request, res: Response) => {
     const series = req.params.series;
     let seriesName = getSeriesName(series);
 
+    const manuf_type =
+        series === "gtwca" || series === "pgt4a"
+            ? "manufPointsList"
+            : "manufPoints";
+
     try {
         const data = await SeriesPoints.findOne({ name: seriesName });
         return data
-            ? res.status(200).json({ seriesData: data?.teamPoints })
+            ? res.status(200).json({ seriesData: data[manuf_type] })
             : res.status(400).json({ message: "Series Not Found" });
     } catch (error) {
         return res.status(500).json({ error });
