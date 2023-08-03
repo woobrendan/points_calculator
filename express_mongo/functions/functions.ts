78f8d0cc0f634412a3be44978e3180ea7b231bed
Brendan Woo <woo.brendan@gmail.com>
2023-08-03 11:41:11 -0400

    add in setNewPoints func and export

--- express_mongo/functions/functions.ts
+++ express_mongo/functions/functions.ts
@@ -1,3 +1,5 @@
+import { PointsInterface } from "../models/Points/points_models";
+
 const getSeriesName = (val: string): string => {
     if (val === "gtwca") return "GT World Challenge America";
     if (val === "pgt4a") return "Pirelli GT4 America";
@@ -8,4 +10,35 @@ const getSeriesName = (val: string): string => {
     return "";
 };
 
-export { getSeriesName };
+const setNewPoints = (round: string, points: number) => {
+    const pts: PointsInterface = {
+        R1: null,
+        R2: null,
+        R3: null,
+        R4: null,
+        R5: null,
+        R6: null,
+        R7: null,
+        R8: null,
+        R9: null,
+        R10: null,
+        R11: null,
+        R12: null,
+        R13: null,
+        R14: null,
+        R15: null,
+        R16: null,
+        R17: null,
+        R18: null,
+    };
+
+    for (const roundNum in pts) {
+        if (roundNum === round) {
+            pts[roundNum] = points;
+        }
+    }
+
+    return pts;
+};
+
+export { getSeriesName, setNewPoints };
