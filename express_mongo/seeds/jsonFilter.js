cb5fe2bc9ea6f434f57938d78a801e123e604b41
Brendan Woo <woo.brendan@gmail.com>
2023-08-08 16:50:12 -0400

    update to return new entry format required. add in values for serieslist and series to loop through each series

--- express_mongo/seeds/jsonFilter.js
+++ express_mongo/seeds/jsonFilter.js
@@ -1,5 +1,15 @@
 const fs = require("fs");
 
+const series = [
+    "GT World Challenge America",
+    "GT America",
+    "Pirelli GT4 America",
+    "TC America",
+    "Toyota GR Cup",
+];
+
+const seriesShort = ["gtwca", "gta", "pgt4a", "tca", "gr"];
+
 fs.readFile("entries23.json", "utf-8", (err, data) => {
     if (err) {
         console.log("Error reading json entries file", err);
@@ -8,17 +18,34 @@ fs.readFile("entries23.json", "utf-8", (err, data) => {
 
     const jsonData = JSON.parse(data);
 
-    const filtered = jsonData.filter(
-        (obj) => obj.series === "GT World Challenge America",
-    );
+    series.forEach((series, index) => {
+        const filtered = jsonData.filter((obj) => obj.series === series);
 
-    fs.writeFile(
-        "gtwca_entries.json",
-        JSON.stringify(filtered, null, 2),
-        (err) => {
-            if (err) {
-                console.log("Error writing json file");
-            }
-        },
-    );
+        const mappedEntries = filtered.map((entry) => {
+            const { number, team, series, driver1, driver2, classificaiton } =
+                entry;
+
+            return {
+                number,
+                teamName: team,
+                driver1: driver1.teamName,
+                ...(driver2 ? { driver2: { ...driver2.name } } : {}),
+                classificaiton,
+                series,
+                events,
+            };
+        });
+
+        const filename = `${seriesShort[index]}_entries.json`;
+
+        fs.writeFile(
+            filename,
+            JSON.stringify(mappedEntries, null, 2),
+            (err) => {
+                if (err) {
+                    console.log("Error writing json file");
+                }
+            },
+        );
+    });
 });
