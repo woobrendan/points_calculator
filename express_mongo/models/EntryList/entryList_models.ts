70bcedcd91220ea0175a7183711ba452c96ad0bc
Brendan Woo <woo.brendan@gmail.com>
2023-08-08 15:44:27 -0400

    update entry interface and create eventslist. comment out events interface

--- express_mongo/models/EntryList/entryList_models.ts
+++ express_mongo/models/EntryList/entryList_models.ts
@@ -1,6 +1,6 @@
-export interface Events {
-    [key: string]: SeriesList;
-}
+// export interface Events {
+//     [key: string]: SeriesList;
+// }
 
 export interface SeriesList {
     [key: string]: Series;
@@ -13,9 +13,15 @@ export interface Series {
 
 export interface Entry {
     number: number;
+    teamName: string;
     driver1: string;
     driver2?: string;
     driver3?: string;
     classification: string;
     series: string;
+    events: EventsList;
+}
+
+export interface EventsList {
+    [key: string]: boolean;
 }
