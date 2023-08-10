ba3b9b52203d740bc6841fa597c4840abcf89c17
Brendan Woo <woo.brendan@gmail.com>
2023-08-10 15:31:12 -0400

    remove old routes use in old project

--- express_mongo/routes/entriesRoutes.ts
+++ express_mongo/routes/entriesRoutes.ts
@@ -3,10 +3,6 @@ import controller from "../controllers/entry_controller";
 
 const router = express.Router();
 
-router.post("/", controller.createEntry);
 router.get("/", controller.getAllEntries);
-router.get("/:entryId", controller.getEntryById);
-router.patch("/:entryId", controller.updateEntry);
-router.delete("/:entryId", controller.deleteEntry);
 
 export = router;
