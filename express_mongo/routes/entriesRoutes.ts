27ef13be72b21845d3ccec8b8ffe6b272f64df39
Brendan Woo <woo.brendan@gmail.com>
2023-08-10 15:54:01 -0400

    add route with param series to get by series

--- express_mongo/routes/entriesRoutes.ts
+++ express_mongo/routes/entriesRoutes.ts
@@ -4,5 +4,6 @@ import controller from "../controllers/entry_controller";
 const router = express.Router();
 
 router.get("/", controller.getAllEntries);
+router.get("/:series", controller.getEntriesBySeries);
 
 export = router;
