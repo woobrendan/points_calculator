b1d4c6e0c31ce8d41b0ce1bc5ac8a1a237ccd8e1
Brendan Woo <woo.brendan@gmail.com>
2023-08-10 15:55:02 -0400

    create controller for get entries by series

--- express_mongo/controllers/entry_controller.ts
+++ express_mongo/controllers/entry_controller.ts
@@ -1,65 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import EntryLists from "../models/EntryList/entryList_schema";
+import { getSeriesName } from "../functions/functions";
 
 const getAllEntries = async (req: Request, res: Response) => {
-    return EntryLists
-        .find()
+    return EntryLists.find()
         .then((entry) => res.status(201).json({ entry }))
         .catch((error) => res.status(500).json({ error }));
 };
 
-// const getEntriesBySeries = async (req: Request, res: Response) => {
-//   const series = req.params.series;
-//   try {
-//     const seriesEntries = await SeriesListModel.findOne({
-//       'series.name': series
-//     });
-//     return seriesEntries.entries
-//       ? res.status(200).json({ seriesEntries.entries })
-//       : res.status(400).json({ message: "Entries Not Found" });
-//   } catch (error) {
-//     return res.status(500).json({ error });
-//   }
-// };
-
-// const getAllEntries = async (req: Request, res: Response) => {
-//   return Entries.find()
-//     .then((entry) => res.status(201).json({ entry }))
-//     .catch((error) => res.status(500).json({ error }));
-// };
-
-// const updateEntry = async (req: Request, res: Response) => {
-//   const entryId = req.params.entryId;
-//   return Entries.findById(entryId)
-//     .then((entry) => {
-//       if (entry) {
-//         entry.set(req.body);
-//         return entry
-//           .save()
-//           .then((entry) => res.status(201).json({ entry }))
-//           .catch((error) => res.status(500).json({ error }));
-//       } else {
-//         res.status(404).json({ message: "Entry not found" });
-//       }
-//     })
-//     .catch((error) => res.status(500).json({ error }));
-// };
-
-// const deleteEntry = (req: Request, res: Response) => {
-//   const entryId = req.params.entryId;
-//   return Entries.findByIdAndDelete(entryId)
-//     .then((entry) =>
-//       entry
-//         ? res.status(201).json({ message: "Deleted" })
-//         : res.status(404).json({ message: "Not Found" }),
-//     )
-//     .catch((error) => res.status(500).json({ error }));
-// };
+const getEntriesBySeries = async (req: Request, res: Response) => {
+    const series = getSeriesName(req.params.series);
+    try {
+        const seriesEntries = await EntryLists.findOne({ name: series });
+        return seriesEntries
+            ? res.status(200).json({ seriesEntries })
+            : res.status(400).json({ message: "Entries Not Found" });
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
+};
 
 export default {
-    // getEntriesBySeries,
+    getEntriesBySeries,
     getAllEntries,
-    // updateEntry,
-    // deleteEntry,
 };
