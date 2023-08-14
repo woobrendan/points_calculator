8c351224aaa63fb7027612017a80a3dba2e36b5a
Brendan Woo <woo.brendan@gmail.com>
2023-08-14 17:04:28 -0400

    update controller to use seriespOints instead of driverpoints model, update drivers byseries to return data.driverPoints

--- express_mongo/controllers/driverPoints_controller.ts
+++ express_mongo/controllers/driverPoints_controller.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
-import DriverPoints from "../models/DriversPoints/driver_schema";
+import SeriesPoints from "../models/Points/seriesPoints_schema";
 import { getSeriesName } from "../functions/functions";
 
 const getAllDrivers = async (req: Request, res: Response) => {
-    return DriverPoints.find()
+    return SeriesPoints.find()
         .then((drivers) => res.status(201).json({ drivers }))
         .catch((error) => res.status(500).json({ error }));
 };
@@ -12,9 +12,9 @@ const getAllDrivers = async (req: Request, res: Response) => {
 const driversBySeries = async (req: Request, res: Response) => {
     const series = getSeriesName(req.params.series);
     try {
-        const seriesDrivers = await DriverPoints.findOne({ name: series });
-        return seriesDrivers
-            ? res.status(200).json({ seriesDrivers })
+        const data = await SeriesPoints.findOne({ name: series });
+        return data
+            ? res.status(200).json({ seriesDrivers: data.driversPoints })
             : res.status(400).json({ message: "Entries Not Found" });
     } catch (error) {
         return res.status(500).json({ error });
