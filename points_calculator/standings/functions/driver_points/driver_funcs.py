bea1fffc5456c67f24782f588d37a7cc5b3d9f96
Brendan Woo <woo.brendan@gmail.com>
2023-08-15 17:44:15 -0400

    import points class, fix dict deconstruction for classification

--- points_calculator/standings/functions/driver_points/driver_funcs.py
+++ points_calculator/standings/functions/driver_points/driver_funcs.py
@@ -1,10 +1,30 @@
+from ..points import Points
+
+
 def sortDriverPoints(driversArr):
     classDrivers = {}
+
     for driver in driversArr:
-        classification = driver.classification
+        classification = driver['classification']
 
         if classDrivers[classification]:
             classDrivers[classification].append(driver)
         else:
             classDrivers[classification] = [driver]
+
     return classDrivers
+
+
+class Driver_Points_Entry:
+    def __init__(self, name: str, classification: str, points_dict: dict):
+        self.name = name
+        self.classification = classification
+        self.points = Points(points_dict)
+        self.total_points = sum(
+            value or 0 for value in self.points.__dict__.values())
+
+        for round_num in self.points.__dict__:
+            if getattr(self.points, round_num) == 0:
+                setattr(self.points, round_num, 0)
+            elif getattr(self.points, round_num) is None:
+                setattr(self.points, round_num, '')
