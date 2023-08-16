4c3d382ce4958d3751b825077998113cf487bfdd
Brendan Woo <woo.brendan@gmail.com>
2023-08-16 17:49:57 -0400

    remove old logic for sorting driver points. reomve unusused imports

--- points_calculator/standings/functions/driver_points/fetch_drivers_points.py
+++ points_calculator/standings/functions/driver_points/fetch_drivers_points.py
@@ -1,6 +1,5 @@
 import requests
-from .. import helpers
-# import json
+from .driver_funcs import sortDriverPoints
 
 
 def fetch_drivers(series):
@@ -10,25 +9,8 @@ def fetch_drivers(series):
     if response.status_code == 201:
         data = response.json()['seriesDrivers']
 
-        entries_by_class = helpers.classEntries(data)
+        entries_by_class = sortDriverPoints(data)
 
-        # Loop through each list for Pro, Pro/Am etc, and alter the list, to be a list of drivers instead of a list of entries. then sort by most points
-        for classification in entries_by_class:
-            drivers = []
-            entry_list = entries_by_class[classification]
-            for entry in entry_list:
-                driver2 = entry.get('driver2')
-                drivers.append(entry['driver1'])
-
-                if driver2:
-                    drivers.append(entry['driver2'])
-
-            sorted_drivers = sorted(
-                drivers, key=lambda x: x['totalPoints'], reverse=True)
-
-            entries_by_class[classification] = sorted_drivers
-
-        # print(json.dumps(entries_by_class, indent=4))
         return entries_by_class
 
     else:
