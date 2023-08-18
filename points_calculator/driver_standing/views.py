055c1b4e60995675f820ec8cc92836d075db8c71
Brendan Woo <woo.brendan@gmail.com>
2023-08-18 15:11:17 -0400

    import getRounds and implement via round_list, pass to front end. comment out other logic temporarily

--- points_calculator/driver_standing/views.py
+++ points_calculator/driver_standing/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render
 from flask import jsonify
 from .functions.fetch_drivers_points import fetch_drivers
 from Utility.series_buttons import get_series_buttons
+from Utility.helpers import getRounds
 
 
 def drivers_standing(request, series):
-    drivers = fetch_drivers(series)
+    # drivers = fetch_drivers(series)
 
     buttons = get_series_buttons('drivers', series)
+    round_list = getRounds(series)
 
     # if the value is a tuple, means fetch failed, and we have status code and none value
-    if isinstance(drivers, tuple):
-        status_code, _ = drivers
-        error_message = f"Failed to fetch data. Status code: { status_code }"
-        return jsonify({"error": error_message}), status_code
+    # if isinstance(drivers, tuple):
+    #     status_code, _ = drivers
+    #     error_message = f"Failed to fetch data. Status code: { status_code }"
+    #     return jsonify({"error": error_message}), status_code
 
-    else:
-        return render(request, 'standing/drivers_standing.html', {
-            'drivers': drivers,
-            'buttons': buttons
-        })
+    # else:
+    return render(request, 'driver_standing/drivers_standing.html', {
+        # 'drivers': drivers,
+        'buttons': buttons,
+        'round_list': round_list
+    })
