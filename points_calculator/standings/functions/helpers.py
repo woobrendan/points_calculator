3fa7cc668ee29b3c41676006e5dfc192b6011c35
Brendan Woo <woo.brendan@gmail.com>
2023-08-18 14:48:00 -0400

    remove getRounds func and move to utility folderr

--- points_calculator/standings/functions/helpers.py
+++ points_calculator/standings/functions/helpers.py
@@ -36,23 +36,9 @@ def getSeriesName(str):
     if str == "tgr":
         return "Toyota GR Cup"
 
-# For table header, row values
-
-
-def getRounds(series):
-    round_list = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9',
-                  'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16', 'R17', 'R18']
-
-    if series == 'gtwca':
-        return round_list[0:13]
-
-    if series == 'tca' or series == 'pgt4a':
-        return round_list[0:14]
-
-    return round_list
-
-
 # Loops through entry values removes key for rounds 14 to 18 depending on series
+
+
 def handle_rounds(series, class_entries):
     if series != 'gta':
         for _, lists in class_entries.items():
