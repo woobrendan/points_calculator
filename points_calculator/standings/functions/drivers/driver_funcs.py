2dc909aba50f9e086435999415e4465eeebef8c7
Brendan Woo <woo.brendan@gmail.com>
2023-09-18 17:36:38 -0400

    move fetch driver functions inside to remove circular import. update fetch_drviers to return data

--- points_calculator/standings/functions/drivers/driver_funcs.py
+++ points_calculator/standings/functions/drivers/driver_funcs.py
@@ -1,4 +1,25 @@
 from Utility.points import Points
+import requests
+# from .fetch_drivers_points import fetch_drivers
+
+# # Fetch drivers for entries NOT for driver points
+
+
+def fetch_drivers(series):
+    url = f'http://localhost:2020/entries/{series}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()['seriesEntries']['entries']
+
+        return data
+
+        # entries_by_class = sortDriverPoints(data)
+
+        # return entries_by_class
+
+    else:
+        return (response.status_code, None)
 
 
 def sortDriverPoints(driversArr):
@@ -35,3 +56,35 @@ class Driver_Points_Entry:
                 setattr(self.points, round_num, 0)
             elif getattr(self.points, round_num) is None:
                 setattr(self.points, round_num, '')
+
+
+def handle_drivers(result_arr, series):
+    drivers = fetch_drivers(series)
+
+    for result in result_arr:
+        for entry in drivers:
+            # Add provision to check if events[eventName] is true
+            if result["Class"] == entry["classification"] and entry["number"] == result["#"]:
+                result["driver1"] = entry["driver1"]
+
+                if series == 'gtwca' or series == 'pgt4a':
+                    result["driver2"] = entry["driver2"]
+
+    return result_arr
+
+
+def fetch_driver_points(series):
+    url = f'http://localhost:2020/api/drivers/{series}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        # returns dict of keys with array vals
+        data = response.json()['seriesDrivers']
+
+        entries_by_class = sortDriverPoints(data)
+
+        return entries_by_class
+
+    else:
+        return (response.status_code, None)
