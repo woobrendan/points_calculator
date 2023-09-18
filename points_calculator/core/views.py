2d71b7b96042897a41671d321e9e8752a263fdc3
Brendan Woo <woo.brendan@gmail.com>
2023-09-18 15:43:17 -0400

    add flex and justify cventer to styling

--- points_calculator/core/views.py
+++ points_calculator/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    styling = 'py-2 px-2 rounded-xl bg-red-200 text-lg hover:bg-red-500'
+    styling = 'py-2 px-2 rounded-xl bg-red-200 text-lg hover:bg-red-500 flex justify-center'
 
     return render(request, 'core/index.html', {
         'styling': styling
