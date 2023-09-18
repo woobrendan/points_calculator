a553d1d988b45e4660c1f59a2b6f90fdb94546e6
Brendan Woo <woo.brendan@gmail.com>
2023-09-18 15:40:56 -0400

    remove standing list variable and key passed to frontend

--- points_calculator/core/views.py
+++ points_calculator/core/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    standing_list = ('Drivers', 'Manufacturer', 'Team')
     styling = 'py-2 px-2 rounded-xl bg-red-200 text-lg hover:bg-red-500'
 
     return render(request, 'core/index.html', {
-        'standing_list': standing_list,
         'styling': styling
     })
