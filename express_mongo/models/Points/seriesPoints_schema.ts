bc8f16c8da94a9cca0ecbf09b1033db86fffa49d
Brendan Woo <woo.brendan@gmail.com>
2023-09-20 16:24:13 -0400

    adjust schema for driverPoints from being an array with explicit points, to being object with type mixed

--- express_mongo/models/Points/seriesPoints_schema.ts
+++ express_mongo/models/Points/seriesPoints_schema.ts
@@ -33,28 +33,10 @@ const seriesPointsSchema: Schema = new Schema({
             },
         },
     ],
-    driversPoints: [
-        {
-            name: { type: String },
-            classification: { type: String },
-            points: {
-                R1: { type: Schema.Types.Mixed },
-                R2: { type: Schema.Types.Mixed },
-                R3: { type: Schema.Types.Mixed },
-                R4: { type: Schema.Types.Mixed },
-                R5: { type: Schema.Types.Mixed },
-                R6: { type: Schema.Types.Mixed },
-                R7: { type: Schema.Types.Mixed },
-                R8: { type: Schema.Types.Mixed },
-                R9: { type: Schema.Types.Mixed },
-                R10: { type: Schema.Types.Mixed },
-                R11: { type: Schema.Types.Mixed },
-                R12: { type: Schema.Types.Mixed },
-                R13: { type: Schema.Types.Mixed },
-                R14: { type: Schema.Types.Mixed },
-            },
-        },
-    ],
+    driversPoints: {
+        type: Schema.Types.Mixed,
+        strict: false,
+    },
 });
 
 export default mongoose.model<SeriesPointsModel>(
