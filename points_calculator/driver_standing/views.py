84db2bc587fd1842cde8b3a4b02c7a5f77066d2a
Brendan Woo <woo.brendan@gmail.com>
2023-09-21 12:49:34 -0400

    adjust drivers var to be fetch driver points, uncomment if instance, and add drivers to param passed to frontend

--- points_calculator/driver_standing/views.py
+++ points_calculator/driver_standing/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 from flask import jsonify
-from .functions.driver_funcs import fetch_drivers
+from .functions.driver_funcs import fetch_driver_points
 from Utility.series_buttons import get_series_buttons
 from Utility.helpers import getRounds
 
 
 def drivers_standing(request, series):
     # Use different function as this gets entries, not driver points
-    drivers = fetch_drivers(series)
+    drivers = fetch_driver_points(series)
 
     buttons = get_series_buttons('drivers', series)
     round_list = getRounds(series)
 
     # if the value is a tuple, means fetch failed, and we have status code and none value
-    # if isinstance(drivers, tuple):
-    #     status_code, _ = drivers
-    #     error_message = f"Failed to fetch data. Status code: { status_code }"
-    #     return jsonify({"error": error_message}), status_code
+    if isinstance(drivers, tuple):
+        status_code, _ = drivers
+        error_message = f"Failed to fetch data. Status code: { status_code }"
+        return jsonify({"error": error_message}), status_code
 
-    # else:
-    return render(request, 'driver_standing/drivers_standing.html', {
-        # 'drivers': drivers,
-        'buttons': buttons,
-        'round_list': round_list
-    })
+    else:
+        return render(request, 'driver_standing/drivers_standing.html', {
+            'drivers': drivers,
+            'buttons': buttons,
+            'round_list': round_list
+        })
