c428646028a05c4613284956e5bde42401162fa4
Brendan Woo <woo.brendan@gmail.com>
2023-09-21 13:15:05 -0400

    remove fetch driver func that retrieved entries

--- points_calculator/driver_standing/functions/driver_funcs.py
+++ points_calculator/driver_standing/functions/driver_funcs.py
@@ -2,6 +2,21 @@ from Utility.points import Points
 import requests
 
 
+class Driver_Points_Entry:
+    def __init__(self, name: str, classification: str, points_dict: dict):
+        self.name = name
+        self.classification = classification
+        self.points = Points(points_dict)
+        self.total_points = sum(
+            value or 0 for value in self.points.__dict__.values())
+
+        for round_num in self.points.__dict__:
+            if getattr(self.points, round_num) == 0:
+                setattr(self.points, round_num, 0)
+            elif getattr(self.points, round_num) is None:
+                setattr(self.points, round_num, '')
+
+
 def sortDriverPoints(driversArr):
     classDrivers = {}
 
@@ -21,38 +36,6 @@ def sortDriverPoints(driversArr):
             class_list.sort(key=lambda x: x.total_points, reverse=True)
 
     return classDrivers
-
-
-class Driver_Points_Entry:
-    def __init__(self, name: str, classification: str, points_dict: dict):
-        self.name = name
-        self.classification = classification
-        self.points = Points(points_dict)
-        self.total_points = sum(
-            value or 0 for value in self.points.__dict__.values())
-
-        for round_num in self.points.__dict__:
-            if getattr(self.points, round_num) == 0:
-                setattr(self.points, round_num, 0)
-            elif getattr(self.points, round_num) is None:
-                setattr(self.points, round_num, '')
-
-
-# # Fetch drivers for entries NOT for driver points
-def fetch_drivers(series):
-    url = f'http://localhost:2020/entries/{series}'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()['seriesEntries']['entries']
-        print('data', data)
-
-        # entries_by_class = sortDriverPoints(data)
-
-        # return entries_by_class
-
-    else:
-        return (response.status_code, None)
 
 
 def fetch_driver_points(series):
