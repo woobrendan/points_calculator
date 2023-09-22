73dfd0377ba10cccb0dcfbdc988357533aacc6c5
Brendan Woo <woo.brendan@gmail.com>
2023-09-22 16:55:05 -0400

    create config file for tailwind using group-focus

---
+++ points_calculator/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: {
+        enabled: false,
+        content: ["./core/templates/core/base.html"],
+    },
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {
+            display: ["group-focus"],
+        },
+    },
+    plugins: [],
+};
